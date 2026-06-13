8b7704fb5227179e53806c519b815eba26850256
Anonymous21-03 <160504530+Anonymous21-03@users.noreply.github.com>
2026-06-13 18:55:54 +0530

    feat: websocket config for git

--- app/services/websocketService.py
+++ app/services/websocketService.py
@@ -1,14 +1,20 @@
 import asyncio
 from fastapi import WebSocket, WebSocketDisconnect
+from app.services.githubService import github_service
 
 class WebSocketService:
     async def websocket(self, ws: WebSocket):
         await ws.accept()
         try:
+            max_id=0
             while True:
-                await ws.send_text("hello")
-                await asyncio.sleep(2)
+                data=await github_service.get_repo_events("rahulpar2103/Github-Dashboard-Backend")
+                filtered_data = [event for event in data if int(event["id"])>max_id]
+                if filtered_data:
+                    max_id=max([int(event["id"]) for event in filtered_data])
+                    await ws.send_json(filtered_data)
+                await asyncio.sleep(10)
         except WebSocketDisconnect:
-            pass
+            print("Client disconnected")
 
 websocket_service = WebSocketService()
